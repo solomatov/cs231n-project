13d4125fe6edf6a1f394aef902d5620fb329e703
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2018-05-21 23:20:41 -0400

    wgan mnist

--- dogsgan/models/wgan_mnist.py
+++ dogsgan/models/wgan_mnist.py
@@ -3,8 +3,9 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 
+from torchvision import datasets, transforms
+
 from dogsgan.infra.runner import TrainingRunner
-from dogsgan.data.dogs import create_dogs_dataset
 
 ALPHA = 0.2
 BATCH_SIZE = 64
@@ -12,7 +13,7 @@ N_CRITIC = 5
 CLIP = 0.01
 LEARNING_RATE = 0.00005
 NOISE_DIM = 100
-BASE_DIM = 128
+BASE_DIM = 64
 WEIGHT_STD = 0.02
 
 
@@ -27,23 +28,20 @@ class Generator(nn.Module):
         self.base = BASE_DIM
         base = self.base
 
-        self.noise_project = nn.Linear(NOISE_DIM, 4 * 4 * base * 8)
-        self.bn0 = nn.BatchNorm1d(4 * 4 * base * 8, affine=True)
-        self.conv1 = nn.ConvTranspose2d(base * 8, base * 4, (5, 5), stride=2, padding=2, output_padding=1)
-        self.bn1 = nn.BatchNorm2d(base * 4, affine=False)
-        self.conv2 = nn.ConvTranspose2d(base * 4, base * 2, (5, 5), stride=2, padding=2, output_padding=1)
-        self.bn2 = nn.BatchNorm2d(base * 2, affine=False)
-        self.conv3 = nn.ConvTranspose2d(base * 2, base, (5, 5), stride=2, padding=2, output_padding=1)
-        self.bn3 = nn.BatchNorm2d(base, affine=False)
-        self.conv4 = nn.ConvTranspose2d(base, 3, (5, 5), stride=2, padding=2, output_padding=1)
+        self.l1 = nn.Linear(NOISE_DIM, 1024)
+        self.bn1 = nn.BatchNorm1d(1024, affine=True)
+        self.l2 = nn.Linear(1024, 7 * 7 * base * 2)
+        self.bn2 = nn.BatchNorm1d(7 * 7 * base * 2)
+        self.convt1 = nn.ConvTranspose2d(base * 2, base, (4, 4), stride=2, padding=1)
+        self.bn3 = nn.BatchNorm2d(base)
+        self.convt2 = nn.ConvTranspose2d(base, 1, (4, 4), stride=2, padding=1)
 
     def forward(self, z):
-        z0 = lrelu(self.bn0(self.noise_project(z)).view(-1, self.base * 8, 4, 4))
-        z1 = lrelu(self.bn1(self.conv1(z0)))
-        z2 = lrelu(self.bn2(self.conv2(z1)))
-        z3 = lrelu(self.bn3(self.conv3(z2)))
-        z4 = lrelu(self.conv4(z3))
-        return F.tanh(z4)
+        z1 = lrelu(self.bn1(self.l1(z)))
+        z2 = lrelu(self.bn2(self.l2(z1)))
+        z3 = z2.view(-1, self.base * 2, 7, 7)
+        z4 = lrelu(self.bn3(self.convt1(z3)))
+        return F.tanh(self.convt2(z4))
 
 
 class Critic(nn.Module):
@@ -53,24 +51,25 @@ class Critic(nn.Module):
         self.base = BASE_DIM
         base = self.base
 
-        self.conv1 = nn.Conv2d(3, base, (5, 5), padding=2, stride=2)
-        self.bn1 = nn.BatchNorm2d(base, affine=False)
-        self.conv2 = nn.Conv2d(base, base * 2, (5, 5), padding=2, stride=2)
-        self.bn2 = nn.BatchNorm2d(base * 2, affine=False)
-        self.conv3 = nn.Conv2d(base * 2, base * 4, (5, 5), padding=2, stride=2)
-        self.bn3 = nn.BatchNorm2d(base * 4, affine=False)
-        self.conv4 = nn.Conv2d(base * 4, base * 8, (5, 5), padding=2, stride=2)
-        self.bn4 = nn.BatchNorm2d(base * 8, affine=False)
-        self.collapse = nn.Linear((base * 8) * 4 * 4, 1)
+        self.conv1 = nn.Conv2d(1, base, (5, 5))
+        self.conv2 = nn.Conv2d(base, base * 2, (5, 5))
+        self.l1 = nn.Linear(4 * 4 * base * 2, 1024)
+        self.l2 = nn.Linear(1024, 1)
 
         self.clip()
 
     def forward(self, x):
-        z1 = lrelu(self.bn1(self.conv1(x)))
-        z2 = lrelu(self.bn2(self.conv2(z1)))
-        z3 = lrelu(self.bn3(self.conv3(z2)))
-        z4 = lrelu(self.bn4(self.conv4(z3)))
-        return self.collapse(z4.view(-1, (self.base * 8) * 4 * 4))
+
+        N = x.shape[0]
+
+        z1 = lrelu(self.conv1(x))
+        z2 = F.max_pool2d(z1, (2, 2), stride=2)
+        z3 = lrelu(self.conv2(z2))
+        z4 = F.max_pool2d(z3, (2, 2), stride=2)
+        z5 = z4.view(N, -1)
+        z6 = lrelu(self.l1(z5))
+        score = self.l2(z6)
+        return score
 
     def clip(self):
         for m in self.modules():
@@ -79,9 +78,12 @@ class Critic(nn.Module):
                     p.data.clamp(-CLIP, CLIP)
 
 
+mnist = datasets.MNIST(root='data/mnist', download=True, transform=transforms.ToTensor())
+
+
 class WGANTrainingRunner(TrainingRunner):
     def __init__(self):
-        super().__init__('wgan', create_dogs_dataset())
+        super().__init__('wgan-mnist', mnist)
         self.gen = Generator().to(self.device)
         self.critic = Critic().to(self.device)
 
@@ -98,8 +100,7 @@ class WGANTrainingRunner(TrainingRunner):
                     self.critic.zero_grad()
                     self.gen.zero_grad()
                     X_real = X_real.to(self.device)
-                    noise = torch.randn((X_real.shape[0], NOISE_DIM)).to(self.device)
-                    X_fake = self.gen(noise)
+                    X_fake = self.gen(torch.randn((X_real.shape[0], NOISE_DIM)).to(self.device))
                     critic_f = self.critic(X_fake)
                     critic_r = self.critic(X_real)
                     critic_loss = torch.mean(critic_f) - torch.mean(critic_r)
@@ -109,7 +110,7 @@ class WGANTrainingRunner(TrainingRunner):
 
                 self.critic.zero_grad()
                 self.gen.zero_grad()
-                # noise = torch.randn((BATCH_SIZE, NOISE_DIM)).to(self.device)
+                noise = torch.randn((BATCH_SIZE, NOISE_DIM)).to(self.device)
                 gen_loss = -torch.mean(self.critic(self.gen(noise)))
                 gen_loss.backward()
                 self.gen_opt.step()
@@ -134,3 +135,4 @@ class WGANTrainingRunner(TrainingRunner):
 if __name__ == '__main__':
     runner = WGANTrainingRunner()
     runner.run(batch_size=BATCH_SIZE)
+

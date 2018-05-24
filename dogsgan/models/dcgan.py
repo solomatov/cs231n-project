99ae3736dc6b627327de71156a616a68c3aeaa0e
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2018-05-24 09:14:14 -0400

    bn in dcgan

--- dogsgan/models/dcgan.py
+++ dogsgan/models/dcgan.py
@@ -10,15 +10,10 @@ from dogsgan.infra.runner import TrainingRunner
 ALPHA = 0.2
 BATCH_SIZE = 128
 NOISE_DIM = 100
-LABEL_NOISE = 0.00
 BASE_DIM = 128
 WEIGHT_STD = 0.02
 BASE_LR_128 = 0.0002
 GRADIENT_CLIP = 10.0
-
-ZERO_LABEL_MEAN = 2 * LABEL_NOISE
-ONE_LABEL_MEAN = 1.0 - 2 * LABEL_NOISE
-
 
 def lrelu(x):
     return F.leaky_relu(x, ALPHA)
@@ -33,11 +28,6 @@ def init_weights(module):
         init_weight(c)
 
 
-def gen_labels(shape, mean):
-    result = torch.randn(shape) * WEIGHT_STD + mean
-    return result.clamp(min=1e-7, max=1.0)
-
-
 class Generator(nn.Module):
     def __init__(self):
         super().__init__()
@@ -46,18 +36,22 @@ class Generator(nn.Module):
         base = self.base
 
         self.noise_project = nn.Linear(NOISE_DIM, 4 * 4 * base * 8)
+        self.bn0 = nn.BatchNorm1d(4 * 4 * base * 8)
         self.conv1 = nn.ConvTranspose2d(base * 8, base * 4, (5, 5), stride=2, padding=2, output_padding=1)
+        self.bn1 = nn.BatchNorm2d(base * 4)
         self.conv2 = nn.ConvTranspose2d(base * 4, base * 2, (5, 5), stride=2, padding=2, output_padding=1)
+        self.bn2 = nn.BatchNorm2d(base * 2)
         self.conv3 = nn.ConvTranspose2d(base * 2, base, (5, 5), stride=2, padding=2, output_padding=1)
+        self.bn3 = nn.BatchNorm2d(base)
         self.conv4 = nn.ConvTranspose2d(base, 3, (5, 5), stride=2, padding=2, output_padding=1)
 
         init_weights(self)
 
     def forward(self, z):
-        z0 = lrelu(self.noise_project(z).view(-1, self.base * 8, 4, 4))
-        z1 = lrelu(self.conv1(z0))
-        z2 = lrelu(self.conv2(z1))
-        z3 = lrelu(self.conv3(z2))
+        z0 = lrelu(self.bn0(self.noise_project(z)).view(-1, self.base * 8, 4, 4))
+        z1 = lrelu(self.bn1(self.conv1(z0)))
+        z2 = lrelu(self.bn2(self.conv2(z1)))
+        z3 = lrelu(self.bn3(self.conv3(z2)))
         z4 = self.conv4(z3)
         return F.tanh(z4)
 
@@ -70,19 +64,24 @@ class Discriminator(nn.Module):
         base = self.base
 
         self.conv1 = nn.Conv2d(3, base, (5, 5), padding=2, stride=2)
+        self.bn1 = nn.BatchNorm2d(base)
         self.conv2 = nn.Conv2d(base, base * 2, (5, 5), padding=2, stride=2)
+        self.bn2 = nn.BatchNorm2d(base * 2)
         self.conv3 = nn.Conv2d(base * 2, base * 4, (5, 5), padding=2, stride=2)
+        self.bn3 = nn.BatchNorm2d(base * 4)
         self.conv4 = nn.Conv2d(base * 4, base * 8, (5, 5), padding=2, stride=2)
+        self.bn4 = nn.BatchNorm2d(base * 8)
         self.collapse = nn.Linear((base * 8) * 4 * 4, 1)
 
         init_weights(self)
 
     def forward(self, x):
-        z1 = lrelu(self.conv1(x))
-        z2 = lrelu(self.conv2(z1))
-        z3 = lrelu(self.conv3(z2))
-        z4 = lrelu(self.conv4(z3))
-        return F.sigmoid(self.collapse(z4.view(-1, (self.base * 8) * 4 * 4)))
+        z1 = lrelu(self.bn1(self.conv1(x)))
+        z2 = lrelu(self.bn2(self.conv2(z1)))
+        z3 = lrelu(self.bn3(self.conv3(z2)))
+        z4 = lrelu(self.bn4(self.conv4(z3)))
+        z5 = self.collapse(z4.view(-1, (self.base * 8) * 4 * 4))
+        return F.sigmoid(z5)
 
 
 class DCGANRunner(TrainingRunner):
@@ -109,9 +108,8 @@ class DCGANRunner(TrainingRunner):
             y_fake = torch.zeros((N, 1)).to(self.device)
             y_real = torch.ones((N, 1)).to(self.device)
 
-            X = torch.cat([X_real, X_fake])
             y = torch.cat([y_real, y_fake])
-            y_ = self.dsc(X)
+            y_ = torch.cat([self.dsc(X_real), self.dsc(X_fake)])
 
             dsc_loss = F.binary_cross_entropy(y_, y)
             dsc_loss.backward()
@@ -126,8 +124,8 @@ class DCGANRunner(TrainingRunner):
             gen_loss.backward()
             self.gen_opt.step()
 
-            context.add_scalar('gen_loss', gen_loss)
-            context.add_scalar('dsc_loss', dsc_loss)
+            context.add_scalar('loss/gen', gen_loss)
+            context.add_scalar('loss/dsc', dsc_loss)
 
     def sample_images(self):
         return self.gen(self.vis_params)

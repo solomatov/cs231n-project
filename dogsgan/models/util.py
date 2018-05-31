f26202fcd092eef4381270431838debcb3a92eeb
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2018-05-31 10:24:15 +0200

    better init

--- dogsgan/models/util.py
+++ dogsgan/models/util.py
@@ -3,5 +3,8 @@ import torch.nn as nn
 
 def init_weights(module, std=0.02):
     for m in module.modules():
-        if isinstance(m, nn.Linear) or isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
+        if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
             m.weight.data.normal_(0.0, std)
+        elif isinstance(m, nn.Linear):
+            nn.init.xavier_normal_(m.weight.data)
+

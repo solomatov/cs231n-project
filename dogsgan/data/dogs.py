a5ef46219a6c1a70834466f11bc4cd184fc31b3c
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2018-06-04 13:29:50 +0200

    work on inception score

--- dogsgan/data/dogs.py
+++ dogsgan/data/dogs.py
@@ -2,10 +2,18 @@ from torchvision import datasets, transforms
 from dogsgan.data.common import preprocessed_dir
 
 
-def create_dogs_dataset():
-    trans = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+def create_dogs_dataset(normalize=True, image_transforms=None, additional_transforms=None):
+    trans_list = []
 
-    return datasets.ImageFolder(str(preprocessed_dir), transform=trans)
+    if image_transforms is not None:
+        trans_list.extend(image_transforms)
+
+    trans_list.append(transforms.ToTensor())
+
+    if normalize:
+        trans_list.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
+
+    if additional_transforms is not None:
+        trans_list.extend(additional_transforms)
+
+    return datasets.ImageFolder(str(preprocessed_dir), transform=transforms.Compose(trans_list))
--- dogsgan/data/eval.py
+++ dogsgan/data/eval.py
@@ -1,7 +1,20 @@
 from torchvision.models.inception import inception_v3
+from torchvision import transforms
+from torch.utils.data import DataLoader
 
+
+from dogsgan.data.dogs import create_dogs_dataset
 
 if __name__ == '__main__':
     inception = inception_v3(pretrained=True)
+    inception.train(False)
 
-    print()
+    dogs_dataset = create_dogs_dataset(
+        normalize=False,
+        image_transforms=[transforms.Resize((299, 299))]
+    )
+    dataloader = DataLoader(dogs_dataset, batch_size=16)
+
+    for X, _ in dataloader:
+        out = inception(X)
+        print(out.shape)

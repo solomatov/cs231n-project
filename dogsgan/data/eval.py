4b3a015c0f556bcb761651823d88aae9b7433cce
Konstantin Solomatov <konstantin.solomatov@gmail.com>
2018-06-07 22:14:09 -0400

    refactor eval.py

--- dogsgan/data/eval.py
+++ dogsgan/data/eval.py
@@ -1,12 +1,9 @@
 import torch
 import torch.nn.functional as F
-
-from tqdm import tqdm
-
-from torchvision.models.inception import inception_v3
-from torchvision import transforms
 from torch.utils.data import DataLoader
-
+from torchvision import transforms
+from torchvision.models.inception import inception_v3
+from tqdm import tqdm
 
 from dogsgan.data.dogs import create_dogs_dataset
 
@@ -15,28 +12,34 @@ def kl_divergence(p, q):
     return torch.sum(p * torch.log(p / q))
 
 
+def apply_classifier(classifier, images):
+    has_cuda = torch.cuda.device_count() > 0
+    with torch.no_grad():
+        scores = []
+        for X, _ in tqdm(DataLoader(images, batch_size=64), desc='Computing Marginal Dist'):
+            if has_cuda:
+                X = torch.Tensor(X).cuda()
+            scores.append(F.softmax(classifier(X), 1))
+        scores = torch.cat(scores, 0)
+
+    return scores
+
+
+def compute_marginal(dists):
+    sums = torch.sum(dists, 0)
+    return sums / torch.sum(sums)
+
+
 def inception_score(images):
     inception = inception_v3(pretrained=True)
+    inception.train(False)
 
     has_cuda = torch.cuda.device_count() > 0
     if has_cuda:
         inception = inception.cuda()
 
-    inception.train(False)
-
-    with torch.no_grad():
-        scores = []
-
-        for X, _ in tqdm(DataLoader(images, batch_size=64), desc='Inception Score'):
-            if has_cuda:
-                X = torch.Tensor(X).cuda()
-
-            scores.append(F.softmax(inception(X), 1))
-
-        scores = torch.cat(scores, 0)
-
-    sums = torch.sum(scores, 0)
-    mdist = sums / torch.sum(sums)
+    scores = apply_classifier(inception, images)
+    mdist = compute_marginal(scores)
 
     divs = []
     for i in range(0, scores.shape[0]):
